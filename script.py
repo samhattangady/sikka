11c683e191161f1fb70b128561203a25ebe56350
Samarth Hattangady <samhattangady@gmail.com>
2017-09-29 20:58:27 +0530

    Create Opcodes file.
    The opcodes can now be imported from a separate file.
    The actual functionality of the script is yet to be
    implemented.

--- script.py
+++ script.py
@@ -1,2 +1,16 @@
 class Script(object):
-    pass
+
+    @property
+    def script(self):
+        """
+        The bytes that represent the script to be computed
+        """
+        return self._script
+
+    def __init__(self, script):
+        self._script = script
+        self._stack = []
+
+    def execute(self):
+        pass
+
